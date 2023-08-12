0cc6876210f6ee37bbda0019ad0d9955dd26dd4a
mawadasweis19 <67907899+mawada-sweis@users.noreply.github.com>
2023-08-12 13:44:13 +0300

    Remove stopwords from tokens

--- src/prepare_data.py
+++ src/prepare_data.py
@@ -1,6 +1,10 @@
 from datasets import load_dataset
 import pandas as pd
 import tiktoken
+import nltk
+from nltk.corpus import stopwords
+
+# nltk.download('stopwords')
 
 # Set the model ID to a fixed value for this scenario
 model_id = 'davinci'
@@ -22,6 +26,22 @@ def tokenize_text(text: str, model_ID: str) -> list:
     tokens = [enc.decode_single_token_bytes(token).decode('utf-8').strip() for token in tokens]
     return tokens
 
+
+def remove_stop_words(tokens: list) -> list:
+    """Remove English stopwords from a list of tokens.
+
+    Args:
+        tokens (list): A list of tokens.
+
+    Returns:
+        list: A new list of tokens with stopwords removed.
+    """
+    
+    if 'en_stopwords' not in globals():
+        en_stopwords = set(stopwords.words('english'))
+    return [token for token in tokens if token.lower() not in en_stopwords]
+
+
 dataset = load_dataset('TalTechNLP/AMIsum', split='train+validation+test').to_pandas()
 dataset = pd.DataFrame(dataset.drop(columns='id')) 
 
@@ -29,5 +49,8 @@ dataset['summary_tokens'] = dataset['summary'].apply(tokenize_text, model_ID=mod
 dataset['transcript_tokens'] = dataset['transcript'].apply(tokenize_text, model_ID=model_id)
 
 # Get the talks position tag
-tags = set(transcript_dataset['transcript'].str.findall(r'<(.*?)>').sum())
+tags = set(dataset['transcript'].str.findall(r'<(.*?)>').sum())
 tags = [f'<{tag}>' for tag in tags]
+
+dataset['clean_transcript'] = dataset['transcript_tokens'].apply(remove_stop_words)
+dataset.to_csv('./dataset/cleaned_dataset.csv', index=False)
